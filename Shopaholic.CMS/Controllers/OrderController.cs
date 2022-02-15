7f2aadb202ea3b8eba17cffed505d2f33d64e8e1
Yuhui <acs102145@gmail.com>
2022-02-15 23:50:45 +0800

    order commit

---
+++ Shopaholic.CMS/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shopaholic.CMS.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Shopaholic.Service.Model/Moels/OrderDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopaholic.Service.Model.Moels
+{
+    public class OrderDTO
+    {
+    }
+}
