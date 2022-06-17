4222b4873a841dec94dd7cf1cbe0de4ea6d742a9
Luka Lai <lukalai@starlux-airlines.com>
2022-06-17 13:44:43 +0800

    ServiceCollectionExtension commit

---
+++ Shopaholic.DI.Configuration/DI/ServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopaholic.DI.Configuration.DI
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void ConfigureBusinessService(this IServiceCollection service, IConfiguration configuration)
+        {
+            if(service!=null)
+            {
+                /*service.AddScoped<IProductService, ProductService>();
+                service.AddScoped<ICategoryService, CategoryService>();
+
+                service.AddScoped<IWebFlowService, WebFlowService>();
+                service.AddScoped<IOrderService, OrderService>();*/
+            }
+        }
+    }
+}
